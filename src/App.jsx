734ff7744b7dc75fbcb769c24d2a540f014e6c10
Aminakarfi <karfi.amina7@gmail.com>
2025-06-04 06:52:10 +0100

    Code review

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Landing } from './pages/landing'
-import { Login }  from  './pages/login'
-import { Routes,Route } from 'react-router-dom'
+import Home from './components/Home'
+import {Footer} from './layout/Footer'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-      
+      <Home />
+      <Footer />
     </>
+
   )
 }
 
